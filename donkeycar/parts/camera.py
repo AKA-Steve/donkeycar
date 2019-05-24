8c3491a54c259431c455c8ffc9ef14bd3bc26a47
Jackson Cwach <jackson.cwach@tennantco.com>
2019-05-24 12:31:47 -0500

    Update camera.py
    Added more links to investigate

--- donkeycar/parts/camera.py
+++ donkeycar/parts/camera.py
@@ -8,6 +8,9 @@ import cv2
 
 
 # https://towardsdatascience.com/image-pre-processing-c1aec0be3edf
+# https://docs.opencv.org/3.3.1/d3/db4/tutorial_py_watershed.html
+
+# https://www.hackster.io/kemfic/curved-lane-detection-34f771
 
 class BaseCamera:
 
@@ -27,8 +30,28 @@ class CameraProcessor():
         # Process the image
         #self.processed_image = cv2.Canny(img_arr,100,200)
         blur = cv2.GaussianBlur(img_arr,(5,5),0)
-        #gray = cv2.cvtColor(blur, cv2.COLOR_BGR2GRAY)
-        ret, self.processed_image = cv2.threshold(blur,0,255,cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+        # Unneccesary
+        gray = cv2.cvtColor(blur, cv2.COLOR_BGR2GRAY)
+        
+        ret, thresh = cv2.threshold(gray,0,255,cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+        
+        kernel = np.ones((3,3), np.uint8)
+        opening = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel, iterations = 2)
+        sure_bg = cv2.dilate(opening, kernel, iterations = 3)
+        
+        dist_transform = cv2.distanceTransform(opening, cv2.DIST_L2, 5)
+        ret, sure_fg = cv2.threshold(dist_transform, 0.7*dist_transform.max(), 255, 0)
+        sure_fg = np.uint8(sure_fg)
+        
+        unknown = cv2.subtract(sure_bg,sure_fg)
+        
+        ret, markers = cv2.connectedComponents(sure_fg)
+        markers += 1
+        markers[unknown==255] = 0
+        markers = cv2.watershed(img_arr,markers)
+        img_arr[markers==-1] = [255,0,0]
+        
+        self.processed_image = img_arr
         return self.processed_image
 
     def update(self):
@@ -42,7 +65,7 @@ class CameraProcessor():
         
 class TestCam(BaseCamera):
     def __init__(self, resolution=(120, 160), framerate=20):
-        self.frame = cv2.imread('Neptune_Full.jpg',0)
+        self.frame = cv2.imread('Neptune_Full.jpg')
         
     def run(self):
         return self.frame
